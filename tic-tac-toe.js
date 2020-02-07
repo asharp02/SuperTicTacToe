2ce897447da972ef044154d9975ea91420a8c539
Adrian Sharpe <asharpe@nationaljournal.com>
2020-02-07 16:11:05 -0500

    added additional boards for SUPER tic-tac-toe

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,10 +1,11 @@
 class TicTacToe{
     constructor(id){
-        this.board = document.querySelectorAll(`#${id} td`);
+        this.board = document.querySelector(`#${id} .board`);
+        this.cells = document.querySelectorAll(`#${id} td`);
         this.modal = document.querySelector(`#${id} .modal`);
         this.modalMsg = document.querySelector(`#${id} .modal .result_msg`);
         const self = this;
-        this.board.forEach(function (cell){
+        this.cells.forEach(function (cell){
             cell.addEventListener("click", function(){self.move(cell)})
         });
         this.initializeGame();
@@ -19,12 +20,12 @@ class TicTacToe{
         this.is_game_active = true;
     }
     clear_board(){
-        this.board.forEach(function(value){
+        this.cells.forEach(function(value){
             value.innerHTML = "";
         })
-        let board = document.querySelector(".board");
-        board.style.opacity = 1;
+        this.board.style.opacity = 1;
         this.modal.style.display = "none";
+        this.playAgainBtn.style.display = "none";
     }
     place_char(cell){
         if(cell.innerHTML === ""){
@@ -34,9 +35,9 @@ class TicTacToe{
     }
     horizontal_check(){
         for(let row=0; row < 9; row+=3){
-            let char1 = this.board[row].innerHTML;
-            let char2 = this.board[row+1].innerHTML;
-            let char3 = this.board[row+2].innerHTML;
+            let char1 = this.cells[row].innerHTML;
+            let char2 = this.cells[row+1].innerHTML;
+            let char3 = this.cells[row+2].innerHTML;
             if(char1 === char2 && char2 === char3 && char1 !== ""){
                 return true;
             }
@@ -45,9 +46,9 @@ class TicTacToe{
     }
     vertical_check(){
         for(let i=0; i < 3; i++){
-            let char1 = this.board[i].innerHTML;
-            let char2 = this.board[i+3].innerHTML;
-            let char3 = this.board[i+6].innerHTML;
+            let char1 = this.cells[i].innerHTML;
+            let char2 = this.cells[i+3].innerHTML;
+            let char3 = this.cells[i+6].innerHTML;
 
             if(char1 === char2 && char2 === char3 && char1 !== ""){
                 return true;
@@ -57,11 +58,11 @@ class TicTacToe{
 
     }
     diagonal_check(){
-        let lu_corner = this.board[0].innerHTML;
-        let middle = this.board[4].innerHTML;
-        let rl_corner = this.board[8].innerHTML;
-        let ru_corner = this.board[2].innerHTML;
-        let ll_corner = this.board[6].innerHTML;
+        let lu_corner = this.cells[0].innerHTML;
+        let middle = this.cells[4].innerHTML;
+        let rl_corner = this.cells[8].innerHTML;
+        let ru_corner = this.cells[2].innerHTML;
+        let ll_corner = this.cells[6].innerHTML;
 
         if(lu_corner === middle && middle === rl_corner && lu_corner != ""){
             return true
@@ -74,7 +75,7 @@ class TicTacToe{
     is_full(){
         //CHEATING WITH HARDCODED LENGTH!!!
         for(let i=0; i < 9; i++){
-            if(this.board[i].innerHTML === ""){
+            if(this.cells[i].innerHTML === ""){
                 return false;
             }
         }
@@ -82,8 +83,7 @@ class TicTacToe{
     }
     end_game(){
         this.is_game_active = false;
-        let board = document.querySelector(".board");
-        board.style.opacity = 0.2;
+        this.board.style.opacity = 0.2;
     }
     check_board(){
         const winner = this.horizontal_check() || this.vertical_check() || this.diagonal_check();
@@ -105,3 +105,11 @@ class TicTacToe{
 }
 
 board = new TicTacToe("game1");
+board = new TicTacToe("game2");
+board = new TicTacToe("game3");
+board = new TicTacToe("game4");
+board = new TicTacToe("game5");
+board = new TicTacToe("game6");
+board = new TicTacToe("game7");
+board = new TicTacToe("game8");
+board = new TicTacToe("game9");
